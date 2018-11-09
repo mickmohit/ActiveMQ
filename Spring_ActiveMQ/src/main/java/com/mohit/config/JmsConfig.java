0ec16ea6e30aa8b10ac0dc2d5d4559401a62e069
Mohit Darmwal <36398566+mickmohit@users.noreply.github.com>
2018-11-09 13:05:12 +0530

    Add files via upload

---
+++ Spring_ActiveMQ/src/main/java/com/mohit/config/JmsConfig.java
@@ -0,0 +1,50 @@
+package com.mohit.config;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.core.JmsTemplate;
+
+@Configuration
+@EnableJms
+@ComponentScan(basePackages = "com.mohit")
+public class JmsConfig {
+	String BROKER_URL = "tcp://localhost:61616"; 
+	String BROKER_USERNAME = "admin"; 
+	String BROKER_PASSWORD = "admin";
+	
+	@Bean
+	public ActiveMQConnectionFactory connectionFactory(){
+	    ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
+	    connectionFactory.setBrokerURL(BROKER_URL);
+	    connectionFactory.setPassword(BROKER_USERNAME);
+	    connectionFactory.setUserName(BROKER_PASSWORD);
+	    return connectionFactory;
+	}
+
+	@Bean
+	public JmsTemplate jmsTemplate(){
+	    JmsTemplate template = new JmsTemplate();
+	    template.setConnectionFactory(connectionFactory());
+	   
+	    //template.setPubSubDomain(true);
+	  
+	    return template;
+	}
+
+	@Bean
+	public DefaultJmsListenerContainerFactory jmsListenerContainerFactory() {
+	    DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+	    factory.setConnectionFactory(connectionFactory());
+	    factory.setConcurrency("1-1");
+	    
+	    //factory.setPubSubDomain(true);
+	    
+	    return factory;
+	}
+	
+	
+}
